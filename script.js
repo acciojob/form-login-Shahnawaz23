fa367642a20318be3483b9f38d05f3c148a23ab2
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2025-01-27 16:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
-
+	const form = document.querySelector('form');
+        
+        // Get input values
+        const firstName = form.querySelector('input[name="fname"]').value;
+        const lastName = form.querySelector('input[name="lname"]').value;
+        
+        // Alert the full name
+        alert(`${firstName} ${lastName}`);
+        
+        // Prevent form submission
+        return false;
 }
